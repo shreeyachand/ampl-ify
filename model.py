fd3a8baa3a4289e1c41d3596ae710123b4fc884b
Shreeya Chand <76849512+shreeyachand@users.noreply.github.com>
2023-01-28 11:55:16 -0500

    have model creation align with instructions

--- model.py
+++ model.py
@@ -4,15 +4,15 @@ from sklearn.neighbors import NearestNeighbors
 from sklearn.preprocessing import StandardScaler
 import pickle
 
-def create_model():
-    print("Creating model...\n")
-    features = ['popularity', 'danceability', 'energy', 'loudness', 'mode', 'speechiness', 'acousticness', 'instrumentalness', 'liveness', 'valence', 'tempo']
-    df = pd.read_csv('./model/tracks_clean.csv')
-    X = df[features]
-    scaler = StandardScaler()
-    scaled = scaler.fit_transform(X)
-    nn = NearestNeighbors(n_neighbors=10,algorithm='ball_tree')
-    nn.fit(scaled)
-    pickle.dump(nn, open('./model/nnmodel.pkl', 'wb'))
-    df['id'].to_csv('./model/track_ids.csv',index_label=False)
-    print('Exported model and CSV!')
+
+print("Creating model...\n")
+features = ['popularity', 'danceability', 'energy', 'loudness', 'mode', 'speechiness', 'acousticness', 'instrumentalness', 'liveness', 'valence', 'tempo']
+df = pd.read_csv('./model/tracks_clean.csv')
+X = df[features]
+scaler = StandardScaler()
+scaled = scaler.fit_transform(X)
+nn = NearestNeighbors(n_neighbors=10,algorithm='ball_tree')
+nn.fit(scaled)
+pickle.dump(nn, open('./model/nnmodel.pkl', 'wb'))
+df['id'].to_csv('./model/track_ids.csv',index_label=False)
+print('Exported model and CSV!')
